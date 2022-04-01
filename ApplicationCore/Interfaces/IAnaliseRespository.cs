096a2b3b7cf6a70770f5204189a4b94418a4ffdb
Alex Mendes <alexjr_mendess@hotmail.com>
2022-04-01 16:15:01 -0300

    Repository

---
+++ ApplicationCore/Interfaces/IAnaliseRespository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IAnaliseRespository : IRepository<Analise>
+    {
+        //Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId);
+        //Task<IEnumerable<Produto>> ObterProdutosFornecedores();
+        //Task<Produto> ObterProdutoFornecedor(Guid id);
+    }
+}
---
+++ Infrastructure/Repository/AnaliseRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Interfaces;
+using ApplicationCore.Model;
+using Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public class AnaliseRepository : Repository<Analise>, IAnaliseRespository
+    {
+        public AnaliseRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
